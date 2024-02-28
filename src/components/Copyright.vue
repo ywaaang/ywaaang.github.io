cabecabc3bc666cdd8d2f23a406f66630c6e9aa2
ywaaang <yanbin.wyb@gmail.com>
2024-02-28 16:12:13 +0800

    feat

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -5,7 +5,7 @@ const { t } = useI18n()
 <template>
     <div class="mt-48">
         <p>&#169; Copyright {{new Date().getFullYear()}} All rights reserved.</p> 
-        <p>Made with <span class="text-red-400">&hearts;</span> by {{t('app.name')}}.</p> 
+        <p>Made with <span class="text-red-400">&hearts;</span> by {{t('app.name')}}. </p> 
         <p class="text-xs mt-8">{{t('copyright.stack')}} <span class="text-blue-400">Vue3, Typescripts, Tailwind CSS, and Vite</span>.</p> 
     </div>
 </template>
