3a078f7709eaff2fe11f19d5ae3c54aaffaa82f0
Ywaaang <yanbin.wyb@gmail.com>
2024-02-28 15:56:08 +0800

    Update Copyright.vue
    Fix: copyright

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -4,7 +4,7 @@ const { t } = useI18n()
 </script>
 <template>
     <div class="mt-48">
-        <p>&#169; Copyright 2023 All rights reserved.</p> 
+        <p>&#169; Copyright {{new Date().getFullYear()}} All rights reserved.</p> 
         <p>Made with <span class="text-red-400">&hearts;</span> by {{t('app.name')}}.</p> 
         <p class="text-xs mt-8">{{t('copyright.stack')}} <span class="text-blue-400">Vue3, Typescripts, Tailwind CSS, and Vite</span>.</p> 
     </div>
